98104c8dbe306e4521259842ea59e0bc85bca0a6
Randy Van <randyvanprojets@gmail.com>
2017-02-02 20:19:21 -0700

    created the index.js

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/courses', function(){
+  fs.readFile('courses.json', 'utf8', function (err,data){
+    var courses = JSON.parse(data);
+    response.locals = {courses: courses};
+    response.render('courses.ejs');
+  });
+});
+
+app.get('/courses/:id',function(request,response){
+  fs.readFile('courses.json', 'utf8', function(err, data){
+    var coursesParsed = JSON.parse(data);
+    var courses = productsParsed.filter( function (ojb){
+      return obj.id === parseInt(request.params.id);
+    })[0];
+    
+    response.locals = {course: course};
+    response.render('courses.ejs');
+  });
+});
+
 app.listen(8000);
+
